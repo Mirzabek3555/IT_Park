9c9e920e588dad814487c30e3645a9371c01c27c
Mirzabek3555 <xusainovmirzabek1153555@gmail.com>
2025-08-16 11:05:39 +0500

    postgr sql ulandi

--- itpark_project/settings.py
+++ itpark_project/settings.py
@@ -77,10 +77,16 @@ WSGI_APPLICATION = 'itpark_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'itpark',
+        'USER': 'postgres',
+        'PASSWORD': 'Mirzabek3555',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
